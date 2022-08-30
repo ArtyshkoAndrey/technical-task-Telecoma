ba528fa751754e7f1f0dec1d46b92158a89123c5
Fulliton <artyshko.andrey@gmail.com>
2022-08-30 14:56:56 +0700

    Version 6

--- index.php
+++ index.php
@@ -19,31 +19,63 @@ class Task
   }
 
   /**
-   * O(n) Скорось работы аолгоритма. Версия алгоритма решения задачи № 5
+   * O(n) + O(n log2 n) Скорось работы аолгоритма. Версия алгоритма решения задачи № 6
    *
    * @return int
    */
   public function result (): int {
-   $max = 0;
-   $prepareMax = 0;
-   $i = 0;
-   foreach ($this->a as $key => $item) {
-     $prepareMax += $item;
-     $i++;
-     if ($i % $this->k === 0 && $key !== 0) {
-       $max = $prepareMax % 2 === 0 ? max($max, $prepareMax) : $max;
-       $prepareMax -= $this->a[$key + 1 - $this->k];
-       $i--;
-     }
-   }
-   return $max;
+    $max = 0;
+    $prepareMax = 0;
+    $this->shellSort();
+    $i = 0;
+    foreach ($this->a as $key => $item) {
+      $prepareMax += $item;
+      $i++;
+      if ($i % $this->k === 0 && $key !== 0) {
+        $max = $prepareMax % 2 === 0 ? max($max, $prepareMax) : $max;
+        $prepareMax -= $this->a[$key + 1 - $this->k];
+        $i--;
+      }
+    }
+    return $max;
+  }
+
+  /**
+   * Быстрая сортировка методом шелла
+   *
+   * @return void
+   */
+  private function shellSort(): void
+  {
+    $k=0;
+    $count = count($this->a);
+    $gap[0] = (int) ($count / 2);
+
+    while($gap[$k] > 1) {
+      $k++;
+      $gap[$k]= (int)($gap[$k-1] / 2);
+    }
+
+    for($i = 0; $i <= $k; $i++){
+      $step=$gap[$i];
+
+      for($j = $step; $j < $count; $j++) {
+        $temp = $this->a[$j];
+        $p = $j - $step;
+        while($p >= 0 && $temp >  $this->a[$p]) {
+          $this->a[$p + $step] =  $this->a[$p];
+          $p                   -= $step;
+        }
+        $this->a[$p + $step] = $temp;
+      }
+    }
   }
 }
 
-  $task = new Task([9,17,21,14,16,14], 3);
+$task = new Task([9,17,21,14,16,14], 3);
 
-  $answer = $task->result();
-  var_dump($answer);
+$answer = $task->result();
+var_dump($answer);
 
 
 
